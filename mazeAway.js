f3772bed042df7c8f27050316a3574026ddc4aae
andrewlopezcodes <andrewlopezcodes@gmail.com>
2020-05-06 18:16:49 -0400

    mazeAway.js - basic set up of the parameters for Matter, created variables for width and height, installed borderWalls to keep all elements inside the confines of the canvas, created the world.

---
+++ mazeAway.js
@@ -0,0 +1,54 @@
+const {
+  Engine,
+  Render,
+  Runner,
+  World,
+  Bodies,
+} = Matter;
+
+const engine = Engine.create();
+const {
+  world
+} = engine;
+const width = 600;
+const height = 600;
+const render = Render.create({
+  element: document.body,
+  engine: engine,
+  options: {
+    wireframes: false,
+    width,
+    height
+  }
+});
+
+Render.run(render);
+Runner.run(Runner.create(), engine);
+
+
+const shape = Bodies.rectangle(200, 200, 50, 50, {
+  // isStatic: true
+});
+
+World.add(world, shape);
+
+//Border Walls
+//(x, y, width of shape, height of shape)
+
+const borderWalls = [
+
+  Bodies.rectangle(width * 0.5, 0, width, 40, {
+    isStatic: true
+  }),
+  Bodies.rectangle(width * 0.5, height, width, 40, {
+    isStatic: true
+  }),
+  Bodies.rectangle(0, height * 0.5, 40, height, {
+    isStatic: true
+  }),
+  Bodies.rectangle(width, height * 0.5, 40, height, {
+    isStatic: true
+  }),
+];
+
+World.add(world, borderWalls);
